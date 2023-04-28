800d5ad14ca0391f893b6dac170d8ca9fa8d755b
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:35:42 +0300

    Create AbstractFacade.php

---
+++ src/Facades/AbstractFacade.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Facade as LaravelParentFacade;
+
+/**
+ * AbstractFacade
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+abstract class AbstractFacade extends LaravelParentFacade
+{
+
+}
