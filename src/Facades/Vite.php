b0a11e324e9b1e518adccca261a612021c65d148
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:38:19 +0300

    Create Vite.php

---
+++ src/Facades/Vite.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Vite as LaravelParentFacade;
+
+/**
+ * Vite
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Vite extends LaravelParentFacade
+{
+
+}
