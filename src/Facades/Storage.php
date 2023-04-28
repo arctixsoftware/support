c9153b0f8a7d5e6f7a5192a1ad95c94453778d63
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:38:03 +0300

    Create Storage.php

---
+++ src/Facades/Storage.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Storage as LaravelParentFacade;
+
+/**
+ * Storage
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Storage extends LaravelParentFacade
+{
+
+}
