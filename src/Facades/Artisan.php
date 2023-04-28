e2bdb8e4c1af089d2728fc568691fd17d9d1d550
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:35:49 +0300

    Create Artisan.php

---
+++ src/Facades/Artisan.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Artisan as LaravelParentFacade;
+
+/**
+ * Artisan
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Artisan extends LaravelParentFacade
+{
+
+}
