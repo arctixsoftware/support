f568376f03eb0cad2e7ffe3285e84d2f6c83e05a
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:36:25 +0300

    Create Config.php

---
+++ src/Facades/Config.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Config as LaravelParentFacade;
+
+/**
+ * Config
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Config extends LaravelParentFacade
+{
+
+}
