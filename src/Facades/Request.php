6fdd951be551988942f1d76461f48092aa04049f
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:37:44 +0300

    Create Request.php

---
+++ src/Facades/Request.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Request as LaravelParentFacade;
+
+/**
+ * Request
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Request extends LaravelParentFacade
+{
+
+}
