bfec0a978ca37a3edcc2bb589799c13a5927185d
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:37:34 +0300

    Create RateLimiter.php

---
+++ src/Facades/RateLimiter.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\RateLimiter as LaravelParentFacade;
+
+/**
+ * RateLimiter
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class RateLimiter extends LaravelParentFacade
+{
+
+}
