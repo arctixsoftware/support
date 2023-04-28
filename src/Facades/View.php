753b463bf542c3c76b93a5a5b7fc5ba679d0208d
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:38:16 +0300

    Create View.php

---
+++ src/Facades/View.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\View as LaravelParentFacade;
+
+/**
+ * View
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class View extends LaravelParentFacade
+{
+
+}
