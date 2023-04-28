00161c7686586cb343c8a8fc733ffee8b797e673
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:36:28 +0300

    Create Cookie.php

---
+++ src/Facades/Cookie.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Cookie as LaravelParentFacade;
+
+/**
+ * Cookie
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Cookie extends LaravelParentFacade
+{
+
+}
