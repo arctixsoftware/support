f1ea79b96927bf5c35daf1a7894a2b81732d2cf4
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:37:52 +0300

    Create Route.php

---
+++ src/Facades/Route.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Route as LaravelParentFacade;
+
+/**
+ * Route
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Route extends LaravelParentFacade
+{
+
+}
