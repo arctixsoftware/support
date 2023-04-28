9545c3b9a4862f3a92a21b1aa263dc465e89ded0
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:36:39 +0300

    Create DB.php

---
+++ src/Facades/DB.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\DB as LaravelParentFacade;
+
+/**
+ * DB
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class DB extends LaravelParentFacade
+{
+
+}
