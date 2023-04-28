141aa9911889adfcad6c7036d9e9e89d7f05af16
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:36:49 +0300

    Create Gate.php

---
+++ src/Facades/Gate.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Gate as LaravelParentFacade;
+
+/**
+ * Gate
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Gate extends LaravelParentFacade
+{
+
+}
