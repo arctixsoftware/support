9b6aa2ada31abc9d18441a287069f2c09ab0e997
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:36:46 +0300

    Create File.php

---
+++ src/Facades/File.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\File as LaravelParentFacade;
+
+/**
+ * File
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class File extends LaravelParentFacade
+{
+
+}
