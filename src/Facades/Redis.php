0e60df6a62eb921b04be6b5a0be659776c81ec9a
bu0nq <67270989+bu0nq@users.noreply.github.com>
2023-04-28 13:37:40 +0300

    Create Redis.php

---
+++ src/Facades/Redis.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace Arctix\Supporting\Facades;
+
+use Illuminate\Support\Facades\Redis as LaravelParentFacade;
+
+/**
+ * Redis
+ *
+ * @author bu0nq <hello@bu0nq.ru>
+ */
+final class Redis extends LaravelParentFacade
+{
+
+}
